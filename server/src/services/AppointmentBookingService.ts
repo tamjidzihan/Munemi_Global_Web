bb87e0529519e1f5e82c2b3e8a8eaea6370a64d1
tamjidzihan <tamjidzihan@gmail.com>
2025-03-07 04:07:35 +0600

    office 2025-02-07

--- server/src/services/AppointmentBookingService.ts
+++
@@ -1,7 +0,0 @@
-import { AppointmentBookingModel } from "../db/AppointmentBookingModel.";
-
-export const getAppointmentBookings = () => AppointmentBookingModel.find()
-export const findAppointmentBookingByid = (id: string) => AppointmentBookingModel.findById(id)
-export const createAppointmentBooking = (valuse: Record<string, any>) => new AppointmentBookingModel(valuse).save().then((appointment) => appointment.toObject());
-export const updateAppointmentBooking = (id: string, valuse: Record<string, any>) => AppointmentBookingModel.findByIdAndUpdate(id, valuse)
-export const deleteAppointmentBookingById = (id: string) => AppointmentBookingModel.findByIdAndDelete({ _id: id });
