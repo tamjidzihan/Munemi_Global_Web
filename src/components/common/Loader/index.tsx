578fa9077fb6f67dc1a6994b29dcaf36fe82d467
tamjidzihan <tamjidzihan@gmail.com>
2025-03-10 01:01:14 +0600

    home 2025-03-10

--- src/components/common/Loader/index.tsx
+++ src/components/common/Loader/index.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
         <div className="flex h-[80vh] items-center justify-center bg-white">
-            <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent border-red-600"></div>
+            <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-t-transparent border-red-600"></div>
         </div>
     );
 };
