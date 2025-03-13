1a56e1ceffdb12817038267f7b2da33464f51a5d
tamjidzihan <tamjidzihan@gmail.com>
2025-03-13 15:38:56 +0600

    office 2025-03-13

--- src/router/StudentEnquiryRouter.ts
+++ src/router/StudentEnquiryRouter.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { getAllStudentEnquiries, createNewStudentEnquiry, deleteStudentEnquiry, updateStudentEnquiry, getStudentEnquirieyById } from "../controllers/StudentEnquiryController";
 
 export default (router: express.Router) => {
-    router.get('/studentenquiry', getAllStudentEnquiries);
-    router.get('/studentenquiry/:id', getStudentEnquirieyById);
-    router.post('/studentenquiry', createNewStudentEnquiry);
-    router.patch('/studentenquiry/:id', updateStudentEnquiry);
-    router.delete('/studentenquiry/:id', deleteStudentEnquiry);
+    router.get('/student-enquiries', getAllStudentEnquiries);
+    router.get('/student-enquiries/:id', getStudentEnquirieyById);
+    router.post('/student-enquiries', createNewStudentEnquiry);
+    router.patch('/student-enquiries/:id', updateStudentEnquiry);
+    router.delete('/student-enquiries/:id', deleteStudentEnquiry);
 };
