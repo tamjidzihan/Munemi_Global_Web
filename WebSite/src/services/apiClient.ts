25af60ecb2f86ebdf38f39c81da83e076befc607
tamjidzihan <tamjidzihan@gmail.com>
2025-03-17 15:49:31 +0600

    Home 2025-03-17

--- WebSite/src/services/apiClient.ts
+++ WebSite/src/services/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const APICLIENT = import.meta.env.VITE_APICLIENT || "http://localhost:3000";
+
 export default axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: APICLIENT,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
