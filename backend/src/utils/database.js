be74cf283f548e3d579a45f1dd9831ad87de3f7b
tamjidzihan <tamjidzihan@gmail.com>
2025-04-04 22:04:07 +0600

    office 2025-04-04

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
-    "munemi_global",
-    "admin",
-    "admin1234",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
+        host: process.env.DB_HOST,
         dialect: 'mysql',
         logging: false
     }
